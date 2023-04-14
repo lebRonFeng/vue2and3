1733405938800d456a5879ea510b98c0c6af5111
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-04-14 15:24:15 +0800

    'day03,提交props配置相关代码'

---
+++ 03_src_props配置/components/Student.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>学生姓名：{{ name }}</h2>
+    <h2>学生性别：{{ sex }}</h2>
+    <h2>学生年龄：{{ myAge+1 }}</h2>
+    <button @click="updateAge">尝试修改年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'School',
+    data(){
+      console.log(this);
+        return {
+            // name: '张三',
+            // sex: '男',
+            // age: 18,
+            msg: '我是尚硅谷的一个学生',
+            myAge:this.age
+        }
+    },
+    methods: {
+      updateAge(){
+        this.myAge++
+      }
+    },
+    // ①props: ['name', 'sex', 'age','phone'] //简单声明接收
+
+    // ②接收的同时对数据进行限制
+    // props: {
+    //   name:String,
+    //   age:Number,
+    //   sex:String
+    // }
+
+    // ③接收的同时对数据：进行限制+默认值的指定+必要性的限制
+    props: {
+      name:{
+        type: String, //name 的类型为字符串
+        require:true //name是必要的
+      },
+      age:{
+        type: Number,
+        default:99   //默认值
+      },
+      sex:{
+        type: String,
+        require:true 
+      }
+    }
+}
+</script>
--- README.md
+++ README.md
@@ -35,3 +35,25 @@
         打标识：<h1 ref='xxx'>...</h1> 或 <School ref='xxx'></School>
         获取：this.$refs.xxx
 
+## 配置项props
+    功能： 让组件接收外部传过来的数据
+        (1).传递数据：
+            <Demo name="xxx"/>
+        (2).接收数据：
+            第一种方式(只接收)：
+                props: ['name']
+
+            第二种方式(限制类型)
+                props: {
+                    name:Number
+                }
+
+            第三种方式(限制类型、限制必要性、指定默认值)：
+                props:{
+                    name:{
+                        type: String,   //类型
+                        required: true, //必要性
+                        default: '老王', //默认值
+                    }
+                }
+    备注：props是只读的，Vue底层会监测你对props的修改，如果进行了修改，就会发出警告，若业务需求确实需要修改，那么请复制props的内容到data种一份，然后去修改data种的数据。
