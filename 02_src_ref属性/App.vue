83de15bfe0b6e78ebae1fc1aedb6cfd9266ee43b
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-04-14 14:42:21 +0800

    'day02,提交ref相关代码'

---
+++ 02_src_ref属性/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDom">点我输出上方的Dom元素</button>
+    <School ref="sch"/>
+  </div>
+</template>
+
+<script>
+import School from './components/School'
+export default{
+    name: 'APP',
+    components: { School },
+    data(){
+        return {
+            msg: '欢迎学习vue!'
+        }
+    },
+    methods: {
+        showDom(){
+            console.log('@@')
+            console.log(this.$refs);
+            
+        }
+    },
+
+};
+</script>
+
+<style scoped>
+</style>
