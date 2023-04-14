430bf1a2ebdfa41cc38640f087f322e6873290db
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-04-14 16:56:58 +0800

    'day05,提交vue插件相关代码'

---
+++ 05_src_插件/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Student/>
+    <hr>
+    <School/>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+export default{
+    name: 'APP',
+    components: { Student, School },
+    data(){
+        return {
+            msg: '欢迎学习vue!'
+        }
+    },
+    methods: {
+    },
+
+};
+</script>
+
+<style scoped>
+</style>
