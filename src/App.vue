57f00754daa9368b23c24d0587ddb37d602ce164
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-04-14 14:40:33 +0800

    'day01,提交分析脚手架内容'

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <template>
   <div>
-    <School></School>
-    <Student></Student>
+    <h1 v-text="msg" ref="title"></h1>
+    <button ref="btn" @click="showDom">点我输出上方的Dom元素</button>
+    <School ref="sch"/>
   </div>
 </template>
 
 <script>
-import School from "./components/School.vue";
-import Student from "./components/Student.vue";
+import School from './components/School'
+export default{
+    name: 'APP',
+    components: { School },
+    data(){
+        return {
+            msg: '欢迎学习vue!'
+        }
+    },
+    methods: {
+        showDom(){
+            console.log('@@')
+            console.log(this.$refs);
+            
+        }
+    },
 
-export default {
-  name: "App",
-  components: {
-    School,
-    Student,
-  },
 };
 </script>
+
+<style scoped>
+</style>
