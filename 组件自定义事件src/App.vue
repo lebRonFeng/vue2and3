9142b35b5406320a4e819cc40e225ce3d3847427
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-04-26 15:45:16 +0800

    '提交组件自定义事件,回到函数形式、ref、props传递等'

---
+++ 组件自定义事件src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <h1>{{ msg }}</h1>
+    <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
+    <School :getSchoolName="getSchoolName"/>
+    <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第一种写法，使用@或v-on） -->
+    <Student @atguigu.once="getStudentName"/>
+        <!-- 通过父组件给子组件绑定一个自定义事件实现：子给父传递数据（第二种写法，使用ref） -->
+        <!-- <Student ref="student"/> -->
+  </div>
+</template>
+
+<script>
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+export default{
+    name: 'APP',
+    components: { Student, School },
+    data(){
+        return {
+          msg: '你好'
+        }
+    },
+    methods: {
+      getSchoolName(name){
+        console.log('APP收到了学校名：',name);
+      },
+      getStudentName(name,...paramas){
+        console.log('APP收到了学生名：', name,paramas);
+        
+      }
+    },
+    mounted(){
+        // this.$refs.student.$on('atguigu',this.getStudentName) //绑定自定义事件
+        // this.$refs.student.$once('atguigu',this.getStudentName) //绑定自定义事件（一次性）
+    }
+};
+</script>
+
+<style scoped>
+.app{
+  background-color: gray;
+  padding: 5px;
+}
+</style>
