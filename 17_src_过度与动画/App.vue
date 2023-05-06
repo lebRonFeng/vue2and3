d85c9310c893d775342f7d3a265106071858de5b
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-05-06 17:59:34 +0800

    'day17，提交过度与动画案例，day18，提交todoList动画'

---
+++ 17_src_过度与动画/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Test1></Test1>
+    <Test2></Test2>
+    <Test3></Test3>
+  </div>
+</template>
+
+<script>
+import Test1 from "./components/test1.vue";
+import Test2 from "./components/test2.vue";
+import Test3 from "./components/test3.vue";
+export default {
+  name: "App",
+  components: { Test1, Test2, Test3 },
+};
+</script>
+
+<style>
+</style>
---
+++ 17_src_过度与动画/components/test3.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition-group 
+      appear
+      name="animate_animated animate_bounce"
+      enter-active-class="animate_swing"
+      leave-active-class="animate__backOutUp">
+      <h1 v-show="isShow" key="1">你好啊！</h1>
+      <h1 v-show="!isShow" key="2">尚硅谷！</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import 'animate.css';
+export default {
+  name: "test3",
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  background-color: green;
+}
+
+/* 进入的起点/离开的终点 */
+/* .hello-enter,.hello-leave-to {
+  transform: translateX(-100%);
+} */
+/* 进入的终点/离开的起点 */
+/* .hello-enter-to,.hello-leave {
+  transform: translateX(0);
+}
+.hello-enter-to,.hello-leave{
+  transition: 0.5s linear;
+} */
+</style>
