4240ca6fbd79ad206aa7ed02cde85cf2c29e0024
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-05-10 16:08:19 +0800

    'day20,github搜索案例'

--- 20_src_github搜索案例/main.js
+++ 20_src_github搜索案例/main.js
@@ -2,12 +2,8 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
-// 引入插件
-// import VueResource from 'vue-resource'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
-// 使用组件
-// Vue.use(VueResource);
 
 //创建vm
 new Vue({
