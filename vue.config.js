0231d67cd6719df10f6898d8809a45a905a198cc
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-05-10 11:51:11 +0800

    'day19,提交配置代理服务器相关代码'

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,25 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   /*关闭语法检查*/
-  lintOnSave: false
+  lintOnSave: false,
+  // devServer:{
+  //   proxy: 'http://localhost:5000'
+  // }
+  devServer:{
+    proxy: {
+      '/atguigu':{
+        target: 'http://localhost:5000',
+        pathRewrite:{'^/atguigu': ''}
+        // ws: true,  //用于支持websocket
+        // changeOrigin: true //用于控制请求头中的host值
+      },
+      '/demo':{
+        target: 'http://localhost:5001',
+        pathRewrite:{'^/demo': ''}
+        // ws: true,  //用于支持websocket
+        // changeOrigin: true //用于控制请求头中的host值
+      },
+    }
+      
+  }
 })
