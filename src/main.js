9b055c65c566edcc7e310346d1d0264b9883118d
lebron_feng <18292996148@163.com>
2023-05-11 22:13:44 +0800

    '搭建vuex环境'

--- src/main.js
+++ src/main.js
@@ -4,16 +4,22 @@ import Vue from 'vue'
 import App from './App.vue'
 //引入插件
 import vueResource from 'vue-resource'
+// 引入store
+import store from './store'
 //关闭Vue的生产提示
 Vue.config.productionTip = false
 //使用插件
-Vue.use(vueResource)
+Vue.use(vueResource);
+
 
 //创建vm
-new Vue({
+const vm = new Vue({
 	el:'#app',
 	render: h => h(App),
+	store,
 	beforeCreate() {
 		Vue.prototype.$bus = this
 	}
 })
+console.log(vm)
+
