15fcac2e99f1b3ddcdefb23c79456872fda74eda
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-05-17 15:38:37 +0800

    'day41,独享路由守卫'

---
+++ 41_src_独享路由守卫/pages/Home.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h2>我是Home的内容</h2>
+		<div>
+			<ul class="nav nav-tabs">
+				<li>
+					<router-link class="list-group-item" active-class="active" to="/home/news">News</router-link>
+					<router-link class="list-group-item" active-class="active" to="/home/message">Message</router-link>
+				</li>
+			</ul>
+			<keep-alive include="News">
+				<router-view></router-view>
+			</keep-alive>
+
+		</div>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name:'Home',
+	// 	beforeDestroy(){
+    //     console.log('Home组件即将被销毁了')
+    // },
+	// mounted(){
+	// 	console.log('Home组件挂载完毕了',this)
+	// 	window.aboutRoute = this.$route
+    //     window.aboutRouter = this.$router
+	// }
+	}
+</script>
