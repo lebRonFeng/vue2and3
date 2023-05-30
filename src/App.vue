27a161cea24d12843bcc8b65e94e34011201e066
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-05-30 10:30:12 +0800

    'Day4,提交vue3的reactive函数'

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,50 @@
 <template>
   <!-- Vue3组件中的模板结构可以没有根标签 -->
   <h1>一个人的信息</h1>
-  <h2>姓名：{{ name }}</h2>
-  <h2>年龄： {{ age }}</h2>
-  <h2>岗位： {{ job.type }}</h2>
-  <h2>薪水： {{ job.salary }}</h2>
+  <h2>姓名：{{ person.name }}</h2>
+  <h2>年龄： {{ person.age }}</h2>
+  <h2>岗位： {{ person.job.type }}</h2>
+  <h2>薪水： {{ person.job.salary }}</h2>
+  <h2>测试的数据： {{ person.job.a.b.c }}</h2>
+  <h3>爱好：{{ person.hobby }}</h3>
   <button @click="changeInfo">修改人的信息</button>
 </template>
 
 <script>
-import {ref} from 'vue'
+import { reactive } from "vue";
 export default {
-  name: 'App',
-  setup(){
-    let name = ref('张三')
-    let age = ref(18)
-    let job = ref({
-      type: '前端工程师',
-      salary: '30k'
+  name: "App",
+  setup() {
+    //数据
+    let person = reactive({
+      name:'张三',
+      age:18,
+      job:{
+        type:'前端工程师',
+        salary: "30k",
+        a:{
+          b:{
+            c:666
+          }
+        }
+      },
+      hobby:['抽烟','喝酒','烫头']
     })
-
     // 方法
-    function changeInfo(){
-      // name.value = '李四'
-      // age.value = 48
-      // console.log(name,age)
-      job.value.type = 'UI设计师'
-      job.value.salary = '60k'
-      console.log(job.value)
+    function changeInfo() {
+      person.name = '李四'
+      person.age = 48
+      person.job.type = 'UI设计师'
+      person.job.salary = '60k'
+      person.job.a.b.c = 999
+      person.hobby[0] = '学习'
     }
-
 
     // 返回一个对象（常用）
-    return{
-      name,
-      age,
+    return {
+      person,
       changeInfo,
-      job
-    }
-  }
-}
+    };
+  },
+};
 </script>
