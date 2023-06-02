94f1421d72daca2aef8701b724b964f57ff33751
刘辉丰 <liu_huifeng@topsec.com.cn>
2023-06-02 17:39:41 +0800

    'readme'

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -5,17 +5,22 @@
 <h2>姓名：{{ name }}</h2>
 <h2>年龄：{{ age }}</h2>
 <h2>薪资：{{ job.j1.salary }}K</h2>
+<h2 v-if="person.car">座驾信息：{{ person.car }}</h2>
 <button @click="name+='~'">修改姓名</button>
 <button @click="age++">增长年龄</button>
 <button @click="job.j1.salary++">涨薪</button>
+<button @click="showRawPerson">输出最原始的person</button>
+<button @click="addCar">给人添加一台车</button>
+<button @click="person.car.name+='~'">换车名</button>
+<button @click="person.car.price++">换价格</button>
 </template>
 
 <script>
-import { ref,reactive,toRef,toRefs,shallowRef, readonly,shallowReadonly } from "vue";
+import { ref,reactive,toRefs,toRaw,markRaw} from "vue";
 export default {
   name: "Demo",
     setup() {
-      
+      let sum = ref(0)
       let person = reactive({
         name: '张三',
         age:18,
@@ -26,13 +31,22 @@ export default {
         }
       })
 
-      // person = readonly(person)
-      person = shallowReadonly(person)
+      function showRawPerson(){
+        const p = toRaw(person)
+        console.log(p)
+      }
 
-      let sum = ref(0)
+      function addCar(){
+        let car = {name:'奔驰',price:40}
+        person.car = markRaw(car)
+      }
+
       return {
+        sum,
+        person,
         ...toRefs(person),
-        sum
+        showRawPerson,
+        addCar
       };
     },
 };
